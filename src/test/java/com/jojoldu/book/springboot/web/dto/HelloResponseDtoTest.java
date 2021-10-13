d1e170bc75684ec1fc81944609b6130c0088300f
Jeon doohyun <sonlcr1@naver.com>
2021-10-14 00:17:02 +0900

    롬복(getter,setter,생성자 자동생성) 테스트 코드 테스트완료

---
+++ src/test/java/com/jojoldu/book/springboot/web/dto/HelloResponseDtoTest.java
@@ -0,0 +1,22 @@
+package com.jojoldu.book.springboot.web.dto;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class HelloResponseDtoTest {
+
+    @Test
+    public void 롬복_기능_테스트() {
+        //given
+        String name = "test";
+        int amount = 1000;
+
+        //when
+        HelloResponseDto dto = new HelloResponseDto(name, amount);
+
+        //then
+        assertThat(dto.getName()).isEqualTo(name);  // dto.getName()로 "test"가 불러져 오는지 확인, name("test"가들어가있다)과 같은지 확인. 생성자 자동 생성됫는지 확인.
+        assertThat(dto.getAmount()).isEqualTo(amount);
+    }
+}
