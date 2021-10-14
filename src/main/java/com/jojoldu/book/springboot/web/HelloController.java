204865073110a81af2ba2ba0db3f1e96892f64af
Jeon doohyun <sonlcr1@naver.com>
2021-10-14 23:08:37 +0900

    dto (json으로 응답옴) 테스트완료

--- src/main/java/com/jojoldu/book/springboot/web/HelloController.java
+++ src/main/java/com/jojoldu/book/springboot/web/HelloController.java
@@ -1,6 +1,8 @@
 package com.jojoldu.book.springboot.web;
 
+import com.jojoldu.book.springboot.web.dto.HelloResponseDto;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController     // 컨트롤러를 JSON으로 반환하는 컨트롤러로 만들어준다.
@@ -9,5 +11,10 @@ public class HelloController {
     @GetMapping("/hello")   // url /hello로 요청이 오면 문자열 "hello"를 반환.
     public String hello() {
         return "hello";
+    }
+
+    @GetMapping("/hello/dto")
+    public HelloResponseDto helloDto(@RequestParam("name") String name, @RequestParam("amount") int amount) {   // @RequestParam은 외부에서 API로 받은 파라미터를 가져오는 어노테이션.
+        return new HelloResponseDto(name, amount);
     }
 }
--- src/test/java/com/jojoldu/book/springboot/web/HelloControllerTest.java
+++ src/test/java/com/jojoldu/book/springboot/web/HelloControllerTest.java
@@ -10,6 +10,8 @@ import org.springframework.test.web.servlet.MockMvc;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.hamcrest.Matchers.is;
 
 @RunWith(SpringRunner.class)    // @RunWith은 스프링부트 테스트와 JUnit사이에 연결자 역할을 한다.
 @WebMvcTest(controllers = HelloController.class)
@@ -27,5 +29,17 @@ public class HelloControllerTest {
                 .andExpect(content().string(hello));    // 내용검증
 
         // Test코드를 run했을때 Tests passed가 뜨면 .andExpect로 실행한 status().isOk()와 content().string(hello)의 테스트가 성공했다는 뜻이다.
+    }
+    
+    @Test
+    public void helloDto가_리턴된다() throws Exception {
+        String name = "hello";  // 1.
+        int amount = 1000;
+        
+        mvc.perform(get("/hello/dto").param("name",name).param("amount",String.valueOf(amount)))    // param() 요청 파라미터를 임시 지정.name에는 hello가 들어가고 amount에는 1000이 들어감. string값만 사용이 가능하기 때문에 숫자를 string으로 변경해줘야함.
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name", is(name)))    // jsonPath는 json의 응답값을 필드별로 검증 할수 있다. json의 필드 name에 들어가 있는게 1.의 name과 같은지 확인.
+                .andExpect(jsonPath("$.amount",is(amount)));
+                
     }
 }
