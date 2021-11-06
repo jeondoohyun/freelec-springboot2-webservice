67ca128ccb76fcc14b4dc06c7e48c4bbaafa0569
Jeon doohyun <sonlcr1@naver.com>
2021-11-06 18:41:35 +0900

    게시글 수정 완료

--- build/resources/main/static/js/app/index.js
+++ build/resources/main/static/js/app/index.js
@@ -1,9 +1,13 @@
 var main = {
     init : function() {
         var _this = this;
-        $('#btn-save').on('click', function() {
+        $('#btn-save').on('click', function() {     // $('#btn-save').on('click', function() {  은 btn-update란 id를 가진 HTML 엘리먼트에 click 이벤트가 발생 할때 save()을 실행.
             _this.save();
         });
+
+        $('#btn-update').on('click', function() {   // id값 앞에 #가 붙는다.
+            _this.update();
+        })
     },
     save : function() {
         var data = {
@@ -24,7 +28,29 @@ var main = {
         }).fail(function(error) {
             alert(JSON.stringify(error));
         });
+    },
+    update : function() {
+        var data = {
+            title : $('#title').val(),
+            content : $('#content').val()
+        };
+
+        var id = $('#id').val();
+
+        $.ajax({
+            type : 'PUT',   // PostsApiController에 있는 API에서 이미 @PutMapping으로 선언했기 때문에 PUT을 사용해야 한다. REST의 CRUD는 HTTP Method의 다음과 같이 매핑된다. Create-post, Read-get, Update-put, Delete-delete
+            url : '/api/v1/posts/' + id,
+            dataType : 'json',
+            contentType : 'application/json; charset=utf-8',
+            data : JSON.stringify(data)
+        }).done(function() {
+            alert('글이 수정되었습니다.');
+            window.location.href = '/';
+        }).fail(function(error) {
+            alert(JSON.stringify(error));
+        });
     }
 
 };
+
 main.init();
